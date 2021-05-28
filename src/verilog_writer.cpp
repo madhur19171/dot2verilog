895cf06d8e64474a637f1f65e5cddb0b780690db
madhur <madhur@madhur-VivoBook>
2021-05-28 12:38:32 +0530

    init commit

---
+++ src/verilog_writer.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : verilog_writer.cpp
+// Author      : Madhur Kumar
+// Version     :
+// Copyright   : Your copyright notice
+// Description : writes .v files in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
